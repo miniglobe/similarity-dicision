39da7df2a9427cff22e1a29c668b28eba76111f0
miumiu0917 <miumiu917@gmail.com>
2017-09-28 07:03:20 +0000

    画像取得からリサイズ処理まで実装

--- prepare_data.py
+++ prepare_data.py
@@ -2,11 +2,21 @@
 
 import tensorflow
 from google.cloud import datastore
+import urllib.request
+from PIL import Image
+import io
 
 PROJECT_ID = "persian-172808"
 DATA_STORE_KEY_PATH = "/home/vagrant/.json_keys/persian-3a9988725cae.json"
 STORAGE_STORE_KEY_PATH = "/home/vagrant/.json_keys/persian-efe392f65854.json"
 BUCKET_NAME = "persian-172808.appspot.com"
+
+params = {
+  "thumnail_width": 200
+  , "thumnail_height": 200
+  , "width": 128
+  , "height": 128
+}
 
 
 def main():
@@ -14,10 +24,42 @@ def main():
   query = datastore_client.query(kind='blog_data')
   offset = 0
   lst = list(query.fetch(limit=100, offset=offset))
-  while lst:
-    print(len(lst))
-    offset += 100
-    lst = list(query.fetch(limit=100, offset=offset))
+  image = download_image(lst[0]["image_url"])
+  image = crop_image(image)
+  image.save('hogehoge.png')
+  
+  # while lst:
+  #   print(len(lst))
+  #   offset += 100
+  #   lst = list(query.fetch(limit=100, offset=offset))
+
+
+def download_image(url):
+  response = urllib.request.urlopen(url)
+  return Image.open(io.BytesIO(response.read()))
+  
+
+def crop_image(img):
+  img.thumbnail((params["thumnail_width"], params["thumnail_height"]), Image.ANTIALIAS)
+  #横幅、縦幅を抜き出す
+  width, height = img.size
+  #画像サイズを加工
+  square_size = min(img.size)
+
+  if width > height:
+      top = 0
+      bottom = square_size
+      left = (width - square_size) / 2
+      right = left + square_size
+      box = (left, top, right, bottom)
+  else:
+      left = 0
+      right = square_size
+      top = (height - square_size) / 2
+      bottom = top + square_size
+      box = (left, top, right, bottom)
+  img = img.crop(box)
+  return img.resize((params["width"], params["height"]))
 
 
 if __name__ == '__main__':
