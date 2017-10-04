06f9b7e4039ca682bddf6c8e51eb3c95ef4ca0f0
miumiu0917 <miumiu917@gmail.com>
2017-10-04 07:15:24 +0000

    updated

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
+
+import tensorflow as tf
 from models import input_data
 
 
+tf.app.flags.DEFINE_integer('batch_size', 32, """バッチサイズ""")
+
+FLAGS = tf.app.flags.FLAGS
+
+
 def main():
-  data = input_data.get_batch(32)
+  data = input_data.get_batch(FLAGS.batch_size)
 
 
 if __name__ == '__main__':
